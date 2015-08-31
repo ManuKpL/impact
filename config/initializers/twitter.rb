26cd289fcdeea999174184ce34402681e4b7965a
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-31 15:43:30 +0200

    added the global variable to make it work

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-twitter = Twitter::REST::Client.new do |config|
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
   config.consumer_secret = ENV['CONSUMER_SECRET']
   config.access_token = ENV['YOUR_ACCESS_TOKEN']
