af224cf914daeacbd13001bea2d5c065c9d15c12
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-09-01 12:10:27 +0200

    created the candidates table

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831160531) do
+ActiveRecord::Schema.define(version: 20150901100301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "profiles", force: :cascade do |t|
+  create_table "candidates", force: :cascade do |t|
     t.string   "name"
     t.string   "screen_name"
     t.string   "location"
     t.string   "description"
-    t.string   "profile_image_url"
     t.integer  "followers_count"
-    t.integer  "statuses_count"
-    t.integer  "friends_count"
-    t.integer  "listed_count"
+    t.integer  "following_count"
+    t.integer  "listed"
+    t.integer  "tweets_count"
+    t.integer  "favorites_count"
     t.date     "account_creation"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.string   "picture"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
