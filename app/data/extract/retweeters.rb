4814547d0c4a40624ed384c19404e17e929d4e2b
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-09-03 17:41:35 +0200

    refacto

--- app/data/extract/retweeters.rb
+++ app/data/extract/retweeters.rb
@@ -1,20 +1,21 @@
 require 'json'
-require_relative '../../../config/environment.rb'
 
 class RetweetersTop20Mentions
-  def run(screen_name)
-    puts "Running !"
+  def initialize(screen_name)
     @file_path = "app/data/json/#{screen_name}_retweeters.json"
-    create_words_instances(screen_name)
-    puts "Done !"
+    @screen_name = screen_name
   end
 
-  def open_file(filepath)
-    JSON.parse(File.open(filepath).read).first.last
+  def run
+    create_words_instances
+  end
+
+  def open_file
+    JSON.parse(File.open(@file_path).read).first.last
   end
 
   def extract_screen_names
-    retweeters = open_file(@file_path)
+    retweeters = open_file
     screen_name = []
     retweeters.each do |user|
       screen_name << user['screen_name']
@@ -43,13 +44,13 @@ class RetweetersTop20Mentions
     return top_result
   end
 
-  def create_topword_instance(screen_name)
-    candidate = Candidate.find_by_screen_name(screen_name)
+  def create_topword_instance
+    candidate = Candidate.find_by_screen_name(@screen_name)
     Topword.create(candidate_id: candidate.id, data_type: 'retweeters screen name')
   end
 
-  def create_words_instances(screen_name)
-    create_topword_instance(screen_name)
+  def create_words_instances
+    create_topword_instance
     topword = Topword.find_by_data_type('retweeters screen name')
     sorted_and_trunkated.each do |word, frequency|
       Word.create(topword_id: topword.id, content: word, count: frequency)
