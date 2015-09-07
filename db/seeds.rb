6df3122b0eef26eadacf35dde5347e62bd52c6c1
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-09-07 10:59:20 +0200

    modified the seed with every candidate + actions for barto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-# require './app/data/extract/candidate_retweeted'
-# require './app/data/extract/find_top_retweeters'
-# require './app/data/extract/find_top_words'
-# require './app/data/extract/followers_list_infos'
-# require './app/data/extract/tweets_list_infos'
+require './app/data/extract/candidate_retweeted'
+require './app/data/extract/find_top_retweeters'
+require './app/data/extract/find_top_words'
+require './app/data/extract/followers_list_infos'
+require './app/data/extract/tweets_list_infos'
 
-# Candidate.destroy_all
+Candidate.destroy_all
+Word.destroy_all
+Topword.destroy_all
+Interaction.destroy_all
 
 # candidats à récupérer
+
 candidates_h = {
   vpecresse: 'Les Républicains',
   claudebartolone: 'Parti socialiste',
@@ -14,9 +18,16 @@ candidates_h = {
   wdesaintjust: 'Front national',
   yannwehrling: 'Mouvement démocrate',
   dupontaignan: 'Debout la France'
+  chantal_jouanno: 'UDI',
+  n_arthaud: "Lutte Ouvrière",
+  delaruejc: "Liste des Usagers",
+  aurelien_veron: "Parti Libéral Démocrate",
+  plaurent_pcf: "Parti Communiste Français",
+  sylvaindesmet: "Indépendant (EELV)"
 }
 
 # récupère les infos du candidat sur Twitter
+
 def get_candidate(screen_name)
   candidate = $twitter.get("https://api.twitter.com/1.1/users/show.json?screen_name=#{screen_name}")
 end
@@ -43,116 +54,108 @@ candidates.each_with_index do |candidate, index|
   candidates_attributes.each { |params| Candidate.create!(params) }
 end
 
+
 # Extraction des informations des JSON
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'retweets',
-#   at_least: 10
-# }).run
+screen_names = %w(claudebartolone)
+screen_names.each do |candidate_screen_name|
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'favorites',
-#   at_least: 5
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'retweets',
+    at_least: 10
+  }).run
 
-# ExtractCandidateRetweeted.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'candidate retweets'
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'favorites',
+    at_least: 5
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'RT retweets',
-#   at_least: 10
-# }).run
+  ExtractCandidateRetweeted.new({
+    screen_name: candidate_screen_name,
+    data_type: 'candidate retweets'
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'RT favorites',
-#   at_least: 5
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'RT retweets',
+    at_least: 10
+  }).run
 
-# ExtractFollowersListInfo.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'followers followers',
-#   at_least: 1000
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'RT favorites',
+    at_least: 5
+  }).run
 
-# ExtractFollowersListInfo.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'followers listed',
-#   at_least: 20
-# }).run
+  ExtractFollowersListInfo.new({
+    screen_name: candidate_screen_name,
+    data_type: 'followers followers',
+    at_least: 1000
+  }).run
 
-# ExtractFollowersListInfo.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'followers tweets',
-#   at_least: 2000
-# }).run
+  ExtractFollowersListInfo.new({
+    screen_name: candidate_screen_name,
+    data_type: 'followers listed',
+    at_least: 20
+  }).run
 
-# ExtractFollowersListInfo.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'followers followings',
-#   at_least: 1000
-# }).run
+  ExtractFollowersListInfo.new({
+    screen_name: candidate_screen_name,
+    data_type: 'followers tweets',
+    at_least: 2000
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'mentions',
-#   top_size: 20
-# }).run
+  ExtractFollowersListInfo.new({
+    screen_name: candidate_screen_name,
+    data_type: 'followers followings',
+    at_least: 1000
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'RT mentions',
-#   top_size: 20
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'mentions',
+    top_size: 20
+  }).run
 
-# FindTopWords.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'words',
-#   content_type: 'tweets',
-#   top_size: 20
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'RT mentions',
+    top_size: 20
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'hashtags',
-#   top_size: 20
-# }).run
+  FindTopWords.new({
+    screen_name: candidate_screen_name,
+    data_type: 'words',
+    content_type: 'tweets',
+    top_size: 20
+  }).run
 
-# ExtractTweetsListInfos.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'RT hashtags',
-#   top_size: 20
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'hashtags',
+    top_size: 20
+  }).run
 
-# FindTopWords.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'retweeters bios',
-#   content_type: 'retweeters',
-#   top_size: 20
-# }).run
+  ExtractTweetsListInfos.new({
+    screen_name: candidate_screen_name,
+    data_type: 'RT hashtags',
+    top_size: 20
+  }).run
 
-# FindTopRetweeters.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'retweeters identity',
-#   content_type: 'screen_name',
-#   top_size: 20
-# }).run
+  ExtractCandidateRetweeted.new({
+    screen_name: candidate_screen_name,
+    data_type: 'candidate retweeted',
+    content_type: 'screen_name',
+    top_size: 20
+  }).run
 
-# ExtractCandidateRetweeted.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'candidate retweeted',
-#   content_type: 'screen_name',
-#   top_size: 20
-# }).run
+  FindTopWords.new({
+    screen_name: candidate_screen_name,
+    data_type: 'followers bios',
+    content_type: 'followers',
+    top_size: 20
+  }).run
 
-# FindTopWords.new({
-#   screen_name: 'claudebartolone',
-#   data_type: 'followers bios',
-#   content_type: 'followers',
-#   top_size: 20
-# }).run
+end
