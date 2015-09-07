71757cb511b3f79c371985f1a89669c1343b1e49
antonin archer <antonin.archer@hec.edu>
2015-09-07 15:10:57 +0200

    added topwords, top users retweeted and top_hashtags to show_view of barto

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -10,8 +10,16 @@ class CandidatesController < ApplicationController
     results[index + 1].nil? ? @next_id = results.first : @next_id = results[index + 1]
     @previous_id = results[index - 1]
 
-    # topwords
+    # Mots présents dans les bios des followers du candidat
     @topwords_bio = @candidate.topwords.where(data_type: "followers bios").first.words
+
+    # Personnes retweetées le plus souvent par le candidat
+    @users_retweeted_by_candidate = @candidate.topwords.where(data_type: "candidate retweeted").first.words
+
+    # Hashtags présents dans les tweets du candidat
+    @hashtags_in_candidate_tweets= @candidate.topwords.where(data_type: "hashtags").first.words
+
+
 
   end
 
