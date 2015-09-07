b6c91c34fcb7dd92444929429833b8d61501c86a
antonin archer <antonin.archer@hec.edu>
2015-09-07 17:19:54 +0200

    added retweets and favs

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -11,25 +11,25 @@ class CandidatesController < ApplicationController
     results[index + 1].nil? ? @next_id = results.first : @next_id = results[index + 1]
     @previous_id = results[index - 1]
 
+    # TOPWORDS
     # top words in candidates bios
     @topwords_bio = @candidate.topwords.where(data_type: "followers bios").first.words
-
     # top mentions in candidate tweets
     @top_candidate_mentions = @candidate.topwords.where(data_type: "mentions").first.words
-
     # top mentions in candidate retweets
     @top_rt_mentions = @candidate.topwords.where(data_type: "RT mentions").first.words
-
     # top users retweeted by candidate
     @users_retweeted_by_candidate = @candidate.topwords.where(data_type: "candidate retweeted").first.words
-
     # top hashtags in candidate tweets
     @hashtags_in_tweets = @candidate.topwords.where(data_type: "hashtags").first.words
-
     # Hashtags in candidate retweets
     @hashtags_in_retweets = @candidate.topwords.where(data_type: "RT hashtags").first.words
 
+    # INTERACTIONS
+    # retweets on candidate tweets
+    @retweets = @candidate.interactions.where(data_type: "retweets").first
 
+    @favoris = @candidate.interactions.where(data_type: "favorites").first
 
   end
 
