9fdac471bee9cfb4ce05e8aa88db85e9c53d2230
vincent pons <pnsvincent@gmail.com>
2015-09-08 15:35:01 +0200

    animated cards and elements mouseover

---
+++ app/assets/javascripts/hatch_hover.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.pulsed').hover(function() {
+    $(this).toggleClass("pulse");
+  });
+  $('.tossed').hover(function() {
+    $(this).toggleClass("tossing");
+  });
+  $('.pulledUp').hover(function() {
+    $(this).toggleClass("pullUp");
+  });
+});
